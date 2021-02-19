c650aba67717b5046778f225e8a5995fcfdf43e7
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-02-19 21:09:53 +0100

    cyclop_matteo update

--- src/cyclop_matteo/cyclop_matteo.py
+++ src/cyclop_matteo/cyclop_matteo.py
@@ -1,70 +1,48 @@
 #!/usr/bin/env python
-"""OpenCV feature detectors with ros CompressedImage Topics in python.
 
-    This example subscribes to a ros topic containing sensor_msgs
-    CompressedImage. It converts the CompressedImage into a numpy.ndarray,
-    then detects and marks features in that image. It finally displays
-    and publishes the new image - again as CompressedImage topic.
-    """
-__author__ = 'Simon Haller <simon.haller at uibk.ac.at>'
-__version__ = '0.1'
-__license__ = 'BSD'
-   # Python libs
 import sys
 import time
 
-   # numpy and scipy
-import numpy as np
-from scipy.ndimage import filters
-
-   # OpenCV
 import cv2
-
-   # Ros libraries
+import numpy as np
 import roslib
 import rospy
-
-   # Ros Messages
+from scipy.ndimage import filters
 from sensor_msgs.msg import CompressedImage
-   # We do not use cv_bridge it does not support CompressedImage in python
-   # from cv_bridge import CvBridge, CvBridgeError
 
 VERBOSE = False
 
+
 class image_feature:
 
-   def __init__(self):
-       '''Initialize ros publisher, ros subscriber'''
-       # topic where we publish
-       self.image_pub = rospy.Publisher("/output/image_raw/compressed",CompressedImage)
-       # self.bridge = CvBridge()
+    def __init__(self):
 
-          # subscribed Topic
-       self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed_image",CompressedImage, self.callback,  queue_size=1)
-       if VERBOSE:
-           print("/raspicam_node/image/compressed_image")
+        self.image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage)
+        
+        self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed_image", CompressedImage, self.callback,  queue_size=1)
+
+        if VERBOSE:
+            print("/raspicam_node/image/compressed_image")
 
     def callback(self, ros_data):
-           '''Callback function of subscribed topic.
-           Here images get converted and features detected'''
-       if VERBOSE:
-           print ('received image of type: "%s"' % ros_data.format)
+
+        if VERBOSE:
+            print('received image of type: "%s"' % ros_data.format)
 
            #### direct conversion to CV2 ####
-       np_arr = np.fromstring(ros_data.data, np.uint8)
-       image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+        np_arr = np.fromstring(ros_data.data, np.uint8)
+        image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
            # image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
 
            #### Feature detectors using CV2 ####
            # "","Grid","Pyramid" +
            # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
-       method = "GridFAST"
-       feat_det = cv2.FeatureDetector_create(method)
-       time1 = time.time()
+        method = "GridFAST"
+        feat_det = cv2.FeatureDetector_create(method)
+        time1 = time.time()
 
            # convert np image to grayscale
-       featPoints = feat_det.detect(
-            cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY))
+        featPoints = feat_det.detect(cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY))
         time2 = time.time()
         if VERBOSE:
             print('%s detector found: %s points in: %s sec.' % (method,
@@ -88,13 +66,14 @@ class image_feature:
         # self.subscriber.unregister()
 
 def main(args):
-    '''Initializes and cleanup ros node'''
+
     ic = image_feature()
     rospy.init_node('cyclop_matteo', anonymous=True)
+    
     try:
         rospy.spin()
     except KeyboardInterrupt:
-        print ("Shutting down ROS Image feature detector module")
+        print("Shutting down ROS Image feature detector module")
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
