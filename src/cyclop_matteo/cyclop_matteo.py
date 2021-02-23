74158ab66450132ae45de42d9cb5fc2d1637529b
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-02-23 17:06:47 +0100

    Update cyclop_matteo.py

--- src/cyclop_matteo/cyclop_matteo.py
+++ src/cyclop_matteo/cyclop_matteo.py
@@ -6,8 +6,9 @@ import cv2
 import numpy as np
 import roslib
 import rospy
+import message_filters
 from scipy.ndimage import filters
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, LaserScan
 
 VERBOSE = True
 
@@ -19,22 +20,30 @@ class image_feature:
         self.image_pub = rospy.Publisher(
             "/output/image_raw/compressed", CompressedImage, queue_size=1)
 
-        self.subscriber = rospy.Subscriber(
-            "/raspicam_node/image/compressed", CompressedImage, self.callback,  queue_size=1)
+        self.image_sub = message_filters.Subscriber(
+            "/raspicam_node/image/compressed", CompressedImage)
+        self.scan_sub = message_filters.Subscriber("/scan", LaserScan)
+
+       # self.subscriber = rospy.Subscriber(
+        #    "/raspicam_node/image/compressed", CompressedImage, self.callback,  queue_size=1)
+
+        ts = message_filters.TimeSynchronizer(
+            [self.image_sub, self.scan_sub], 1)
+        ts.registerCallback(self.callback)
 
         if VERBOSE:
             print("/raspicam_node/image/compressed")
 
-    def callback(self, ros_data):
+    def callback(self, image_sync, scan_sync):
 
         if VERBOSE:
-            print('received image of type: "%s"' % ros_data.format)
+            print('received image of type: "%s"' % image_sync.format)
 
            #### direct conversion to CV2 ####
-        np_arr = np.fromstring(ros_data.data, np.uint8)
+        np_arr = np.fromstring(image_sync.data, np.uint8)
        # image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
-        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
-        image_np = cv2.rotate(image_np,cv2.ROTATE_180)
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # OpenCV >= 3.0:
+        image_np = cv2.rotate(image_np, cv2.ROTATE_180)
 
         #### Feature detectors using CV2 ####
         # "","Grid","Pyramid" +
