51d079aa66c40fd893e525390747e3f6827c0e57
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-03-02 18:21:29 +0100

    Update cyclop_matteo.py

--- src/cyclop_matteo/cyclop_matteo.py
+++ src/cyclop_matteo/cyclop_matteo.py
@@ -129,7 +129,7 @@ class image_feature:
         P = H.dot(Pc)
         UV = np.array([np.divide(P[0, :], P[2, :]),
                        np.divide(P[1, :], P[2, :])], np.uint32)
-        print(len(UV))
+        print(UV.shape)
 
         for u, v in UV:
             if (u <= len(image_np[1, :])) and (v <= len(image_np[1, :])):
