514da388d2d07ef76f24f93f0518b4faa5b557c8
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-03-02 17:42:00 +0100

    Update cyclop_matteo.py

--- src/cyclop_matteo/cyclop_matteo.py
+++ src/cyclop_matteo/cyclop_matteo.py
@@ -129,7 +129,8 @@ class image_feature:
                        np.divide(P[1, :], P[2, :])], np.uint32)
 
         for u, v in UV:
-            if (u < len(image_np[1, :])) and (v < len(image_np[1, :])):
+            if (u <= len(image_np[1, :])) and (v <= len(image_np[1, :])):
+                if (u >= 0) and (v >= 0):
                 cv2.circle(image_np, (int(u), int(v)), 3, (255, 0, 0), -1)
 
         return image_np
