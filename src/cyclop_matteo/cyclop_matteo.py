487db13185bdcc0af073511f903582a24df8dcbc
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-03-02 17:33:07 +0100

    Update cyclop_matteo.py

--- src/cyclop_matteo/cyclop_matteo.py
+++ src/cyclop_matteo/cyclop_matteo.py
@@ -67,6 +67,15 @@ class image_feature:
             x, y = featpoint.pt
             cv2.circle(image_np, (int(x), int(y)), 3, (0, 0, 255), -1)
 
+        ################################################################
+        ###          Adding lidar data to the image                 ####
+        ################################################################
+
+        ranges = self.range_filter(scan_sync)
+        image_np = self.lidar_data_to_img(ranges, image_np)
+
+        ################################################################
+
         cv2.imshow('cv_img', image_np)
         cv2.waitKey(2)
 
@@ -79,6 +88,51 @@ class image_feature:
         self.image_pub.publish(msg)
 
         # self.subscriber.unregister()
+
+    # Filters impossible ranges and combines it with angle data
+    def range_filter(self, scan_sync):
+
+        range_data = np.array(scan_sync.ranges, np.float32)
+        angle_min = scan_sync.angle_min
+        angle_max = scan_sync.angle_max
+        range_min = scan_sync.range_min
+        range_max = scan_sync.range_max
+        angle_increment = scan_sync.angle_increment
+        N = (angle_max-angle_min)/angle_increment
+        angle_data = np.linspace(angle_min, angle_max, num=N)
+
+        ranges = np.array([range_data, angle_data], np.float32)
+
+        ranges[ranges[0, :] > range_max] = range_max
+        ranges[ranges[0, :] < range_min] = range_min
+
+        return ranges
+
+    # Converts lidar range data to XYZ coordinates and then projects it to the camera image plane
+    def lidar_data_to_img(self, ranges, image_np):
+
+        Pl = np.array([np.multiply(np.sin(ranges[1, :]), ranges[0, :]), 0, np.multiply(
+            np.cos(ranges[1, :]), ranges[0, :])], np.float32)
+        # Translation matrix between the camera and the lidar (lidar --> Camera translation) everything in millimeters
+        t = np.array([[0, 50, 52]], np.float32).T
+        # Rotation matrix of the lidar regarding the camera position
+        R = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], np.float32)
+        Pc = R.dot(Pl)+t
+        a = 3.04  # Focal length in millimeters
+        s = 0  # Skew constant of the camera, here 0 'cause the distortion of the camera is already corrected in the raspicam_node
+        u0 = int(len(image_np[1, :])/2)
+        v0 = int(len(image_np[0, :])/2)
+        # Camera plane conversion matrix H
+        H = np.array([[a, s, u0], [0, a, v0], [0, 0, 1]], np.float32)
+        P = H.dot(Pc)
+        UV = np.array([np.divide(P[0, :], P[2, :]),
+                       np.divide(P[1, :], P[2, :])], np.uint32)
+
+        for u, v in UV:
+            if (u < len(image_np[1, :])) and (v < len(image_np[1, :])):
+                cv2.circle(image_np, (int(u), int(v)), 3, (255, 0, 0), -1)
+
+        return image_np
 
 
 def main(args):
