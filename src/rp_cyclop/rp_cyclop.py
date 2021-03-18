888aefcb71505e428df40768f7939427b1307319
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-03-18 17:15:52 +0100

    Update rp_cyclop.py

--- src/rp_cyclop/rp_cyclop.py
+++ src/rp_cyclop/rp_cyclop.py
@@ -189,9 +189,9 @@ class image_feature:
         gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(gray, 50, threshold,
                           apertureSize=3, L2gradient=True)
-        minLineLength = 50
-        maxLineGap = 20
-        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100,
+        minLineLength = 1
+        maxLineGap = 10
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 1,
                                 minLineLength, maxLineGap)
         for x1, y1, x2, y2 in lines[0]:
             cv2.line(image_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
