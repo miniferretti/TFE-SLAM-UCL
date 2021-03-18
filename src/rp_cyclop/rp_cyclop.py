71494bfaa413042da035483eddf3dd2e78d021ae
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-03-18 15:10:01 +0100

    Update rp_cyclop.py

--- src/rp_cyclop/rp_cyclop.py
+++ src/rp_cyclop/rp_cyclop.py
@@ -78,6 +78,14 @@ class image_feature:
 
         ################################################################
 
+        ################################################################
+        ###          Detection of lines in the camera image         ####
+        ################################################################
+
+        lines = self.line_detect(image_np)
+
+        #################################################################
+
         cv2.imshow('cv_img', image_np)
         cv2.waitKey(2)
 
@@ -170,6 +178,17 @@ class image_feature:
 
     def valmap(self, value, istart, istop, ostart, ostop):
         return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+
+    def line_detect(self, image_np):
+        gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+        minLineLength = 100
+        maxLineGap = 10
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100,
+                                minLineLength, maxLineGap)
+        for x1, y1, x2, y2 in lines[0]:
+            cv2.line(image_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        return lines
 
 
 def main(args):
