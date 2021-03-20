702447efbeb219d3065e6cc8fc014c64a8604328
BruNicoletta <bruno6280@gmail.com>
2021-03-20 17:27:56 +0100

    Update rp_cyclop.py

--- src/rp_cyclop/rp_cyclop.py
+++ src/rp_cyclop/rp_cyclop.py
@@ -185,8 +185,8 @@ class image_feature:
     def line_detect(self, image_np):
         global threshold
         gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray, 50, threshold,
-                          apertureSize=3, L2gradient=True)
+        edges = cv2.Canny(gray, 50, threshold=200,
+                          apertureSize=50, L2gradient=True)
         minLineLength = 30
         maxLineGap = 5
         lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=30,
