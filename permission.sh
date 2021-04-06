7ec06a3d51c19ba61596e52fdff8a54fdc2ba822
BruNicoletta <bruno6280@gmail.com>
2021-04-06 17:28:38 +0200

    Update permission.sh

--- permission.sh
+++ permission.sh
@@ -1,3 +1,11 @@
+
 #!/bin/bash
-cd src/cyclop_matteo
-chmod +x cyclop_matteo.py
+cd src/rp_cyclop
+chmod +x rp_cyclop.py
+cd ..
+cd ..
+cd src/monodepth/src
+chmod +x monodepth.py
+cd src/lidepth/src
+chmod +x monodepth.py
+echo "Permission successfuly granted"
