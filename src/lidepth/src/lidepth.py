1fbc95b7acfeb52d3235a17f1ee3421cbb718336
BruNicoletta <bruno6280@gmail.com>
2021-04-09 18:18:44 +0200

    Update lidepth.py

--- src/lidepth/src/lidepth.py
+++ src/lidepth/src/lidepth.py
@@ -221,7 +221,7 @@ class Lidepth:
         data = np.reshape(data, (data.shape[0]*data.shape[1], 1))
         print('data shape is ', data.shape, '\n')
         data = (data[:, 0])
-        data = data.astype(float32)
+        data = data.astype(np.float32)
         print('data shape is ', data.shape, '\n')
 
         for iprinting in range(6):  
