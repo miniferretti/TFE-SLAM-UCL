fd8ed7c283e5d06301a51ef92d1bbb801097a364
BruNicoletta <bruno6280@gmail.com>
2021-04-22 18:54:19 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -211,9 +211,9 @@ class MonoDepth_adabin:
                 r = img[v, u, 2]  # r
                 a = 255
                 if abs(x) < 0.01 and abs(y) < 0.01:
-                    print(x)
-                    print(y)
-                    print(z)
+                   #print(x)
+                   #print(y)
+                   #print(z)
                     b = 255
                     g = 0
                     r = 0
