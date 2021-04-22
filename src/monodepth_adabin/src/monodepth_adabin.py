7b8d3df8ddd1807940ad9a4706cff6e002820755
BruNicoletta <bruno6280@gmail.com>
2021-04-22 18:58:44 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -168,8 +168,8 @@ class MonoDepth_adabin:
                     for inter_u in range(StepWidth):
                     	depth[MidHeight,u_real_previous +inter_u] = depth_previous + StepWidth *(inter_u/StepWidth) * StepDepth
 						for inter_h in range(image_height):
-							interDifferenceDepth = depth[MidHeight,u_real_previous +inter_u] - depth[inter_h, u_real_previous +inter_u]
-                    		depth[inter_h, u_real_previous +inter_u] = depth[inter_h, u_real_previous +inter_u] + interDifferenceDepth *((image_height - abs(MidHeight - inter_h))/image_height)
+						    interDifferenceDepth = depth[MidHeight,u_real_previous +inter_u] - depth[inter_h, u_real_previous +inter_u]
+                    	    depth[inter_h, u_real_previous +inter_u] = depth[inter_h, u_real_previous +inter_u] + interDifferenceDepth *((image_height - abs(MidHeight - inter_h))/image_height)
 
                     # Changes for points with information on x 
                     for hh in range(image_height):
