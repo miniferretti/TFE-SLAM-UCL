3865e6ac4527fe00dfa1c48f77ffecba514e472f
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-05-02 17:40:48 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -268,7 +268,7 @@ class MonoDepth_adabin:
         #    print(e)
 
         np_arr = np.fromstring(image_sync.data, np.uint8)
-        image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
         ranges = self.range_filter(scan_sync)
 
