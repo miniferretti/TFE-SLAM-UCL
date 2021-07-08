c85c7559ddfc71d6944d74b1e82c19bdda8f3a31
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-07-08 14:07:55 +0200

    Update tf_broadcaster_imu.py

--- src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
+++ src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
@@ -5,25 +5,42 @@ import tf2_ros
 import geometry_msgs.msg
 from sensor_msgs.msg import Temperature, Imu
 
+n_samples = 20
+n = 0
+calib = False
+x, y, z, w = 0
+
 
 def handle_imu_pose(msg):
-    br = tf2_ros.TransformBroadcaster()
-    t = geometry_msgs.msg.TransformStamped()
+    global n_samples, n, calib, x, y, z, w
 
-    t.header.stamp = rospy.Time.now()
-    t.header.frame_id = "base_footprint"
-    t.child_frame_id = "base_imu"
-    t.transform.translation.x = 0
-    t.transform.translation.y = 0
-    t.transform.translation.z = 1.1
-    t.transform.rotation.x = msg.orientation.x
-    t.transform.rotation.y = msg.orientation.y
-    t.transform.rotation.z = msg.orientation.z
-    t.transform.rotation.w = msg.orientation.w
+    if calib == False:
+        x += msg.orientation.x
+        y += msg.orientation.y
+        z += msg.orientation.z
+        w += msg.orientation.w
+        n += 1
+        if n == n_samples:
+            calib = True
+    else:
+        br = tf2_ros.TransformBroadcaster()
+        t = geometry_msgs.msg.TransformStamped()
 
-    br.sendTransform(t)
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = "base_footprint"
+        t.child_frame_id = "base_imu"
+        t.transform.translation.x = 0
+        t.transform.translation.y = 0
+        t.transform.translation.z = 1.1
+        t.transform.rotation.x = msg.orientation.x - x/n_samples
+        t.transform.rotation.y = msg.orientation.y - y/n_samples
+        t.transform.rotation.z = msg.orientation.z - z/n_samples
+        t.transform.rotation.w = msg.orientation.w - w/n_samples
+
+        br.sendTransform(t)
+
 
 if __name__ == '__main__':
-      rospy.init_node('tf_broadcaster_imu')
-      rospy.Subscriber('/imu/data', Imu, handle_imu_pose)
-      rospy.spin()
+    rospy.init_node('tf_broadcaster_imu')
+    rospy.Subscriber('/imu/data', Imu, handle_imu_pose)
+    rospy.spin()
