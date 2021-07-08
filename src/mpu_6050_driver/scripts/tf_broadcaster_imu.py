3c8fdc384a8e45eebb4c5b5c7cf5837ca8232679
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-07-08 16:17:22 +0200

    Update tf_broadcaster_imu.py

--- src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
+++ src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
@@ -12,46 +12,47 @@ from sensor_msgs.msg import Temperature, Imu
 n_samples = 20
 n = 0
 calib = False
-x, y, z, w = [0, 0, 0, 0]
+x, y, z = [[], [], []]
+rot = None
+
+
+def average(lst):
+    return sum(lst)/len(lst)
 
 
 def handle_imu_pose(msg):
-    global n_samples, n, calib, x, y, z, w
+    global n_samples, n, calib, x, y, z, w, rot
 
-    if calib == True:
-        x += msg.orientation.x
-        y += msg.orientation.y
-        z += msg.orientation.z
-        w += msg.orientation.w
+    if n < n_samples:
+        a = euler_from_quaternion(
+            msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
+        x.append(a[0])
+        y.append(a[1])
+        z.append(a[2])
         n += 1
-        if n == n_samples + 1:
-            calib = True
-    else:
 
-        br = tf2_ros.TransformBroadcaster()
-        t = geometry_msgs.msg.TransformStamped()
+    br = tf2_ros.TransformBroadcaster()
+    t = geometry_msgs.msg.TransformStamped()
 
-        t.header.stamp = rospy.Time.now()
-        t.header.frame_id = "base_footprint"
-        t.child_frame_id = "base_imu"
-        t.transform.translation.x = 0
-        t.transform.translation.y = 0
-        t.transform.translation.z = 1.1
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "base_footprint"
+    t.child_frame_id = "base_imu"
+    t.transform.translation.x = 0
+    t.transform.translation.y = 0
+    t.transform.translation.z = 1.1
 
-        the_norm = math.sqrt((msg.orientation.x - x/n_samples)**2 + (msg.orientation.y - y/n_samples)
-                             ** 2 + (msg.orientation.z - z/n_samples)**2 + (msg.orientation.w - w/n_samples)**2)
+    q_rot = quaternion_from_euler(
+        0-average(x), math.pi-average(y), 0-average(z))
 
-        q_rot = quaternion_from_euler(0, math.pi, 0)
+    q_or = [msg.orientation.x, msg.orientation.y,
+            msg.orientation.z, msg.orientation.w]
 
-        q_or = [(msg.orientation.x - x/n_samples) / the_norm, (msg.orientation.y - y/n_samples) / the_norm,
-                (msg.orientation.z - z/n_samples) / the_norm, (msg.orientation.w - w/n_samples) / the_norm]
+    q_f = quaternion_multiply(q_rot, q_or)
 
-        q_f = quaternion_multiply(q_rot, q_or)
+    t.transform.rotation = geometry_msgs.msg.Quaternion(
+        q_f[0], q_f[1], q_f[2], q_f[3])
 
-        t.transform.rotation = geometry_msgs.msg.Quaternion(
-            q_f[0], q_f[1], q_f[2], q_f[3])
-
-        br.sendTransform(t)
+    br.sendTransform(t)
 
 
 if __name__ == '__main__':
