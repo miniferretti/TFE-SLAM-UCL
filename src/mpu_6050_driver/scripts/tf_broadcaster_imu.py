fbcfd3d313d3de37849e7a74aa89ffce510e2475
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-07-08 15:42:25 +0200

    Update tf_broadcaster_imu.py

--- src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
+++ src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
@@ -35,6 +35,7 @@ def handle_imu_pose(msg):
         t = geometry_msgs.msg.TransformStamped()
 
         q_rot = quaternion_from_euler(0, math.pi, 0)
+        q_rot = geometry_msgs.msg.Quaternion(q_rot)
 
         t.header.stamp = rospy.Time.now()
         t.header.frame_id = "base_footprint"
