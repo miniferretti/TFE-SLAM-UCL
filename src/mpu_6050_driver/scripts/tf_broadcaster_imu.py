d8e1b54c6653e1dc3e0aff187769559cd50bb4e1
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-07-08 15:24:31 +0200

    Update tf_broadcaster_imu.py

--- src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
+++ src/mpu_6050_driver/scripts/tf_broadcaster_imu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 import tf_conversions
+from tf.transformations import *
 import tf2_ros
 import math
 import numpy as np
@@ -32,6 +33,7 @@ def handle_imu_pose(msg):
         br = tf2_ros.TransformBroadcaster()
         t = geometry_msgs.msg.TransformStamped()
 
+        q_rot = quaternion_from_euler(0, math.pi, 0)
         t.header.stamp = rospy.Time.now()
         t.header.frame_id = "base_footprint"
         t.child_frame_id = "base_imu"
@@ -39,9 +41,10 @@ def handle_imu_pose(msg):
         t.transform.translation.y = 0
         t.transform.translation.z = 1.1
         t.transform.rotation.x = (msg.orientation.x - x/n_samples) / the_norm
-        t.transform.rotation.y = -(msg.orientation.y - y/n_samples) / the_norm
+        t.transform.rotation.y = (msg.orientation.y - y/n_samples) / the_norm
         t.transform.rotation.z = (msg.orientation.z - z/n_samples) / the_norm
         t.transform.rotation.w = (msg.orientation.w - w/n_samples) / the_norm
+        t.transform.rotation = quaternion_multiply(q_rot, t.transform.rotation)
         print(t.transform.rotation)
 
         br.sendTransform(t)
