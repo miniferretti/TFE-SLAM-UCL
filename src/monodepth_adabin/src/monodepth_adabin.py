86d568b1024a98c355607eb643943a61199c98f6
BruNicoletta <bruno6280@gmail.com>
2021-07-15 17:24:59 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -377,6 +377,9 @@ class MonoDepth_adabin:
             cv2.imshow("Image", image)
             cv2.waitKey(1)
 
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+
         # Get image data as a numpy array to be passed for processing.
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
