3a6da6d2fb7984087bfa3992c2f572220e480835
BruNicoletta <bruno6280@gmail.com>
2021-07-21 14:27:43 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -257,7 +257,7 @@ class MonoDepth_adabin:
                         for inter_u in range(abs(StepWidth)):
                             for inter_h in range(image_height):
                                 if ((u_real_previous - inter_u) < 640):
-                                    if(abs(depth[v_real, u_real] - depth[inter_h, u_real_previous - inter_u ]) <= 0.5):
+                                    if(abs(depth[v_real, u_real] - depth[inter_h, u_real_previous - inter_u ]) <= 0.1):
                                         depth[inter_h, u_real_previous - inter_u] = P[2, i]
 
                     #math.copysign(inter_u, StepWidth)
