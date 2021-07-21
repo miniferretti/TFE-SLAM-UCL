953417ec0192f5de8675e67b6124b7539b45f57e
BruNicoletta <bruno6280@gmail.com>
2021-07-21 14:10:53 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -256,8 +256,8 @@ class MonoDepth_adabin:
                     if(correctionMethod == 2):
                         for inter_u in range(abs(StepWidth)):
                             for inter_h in range(image_height):
-                                if (abs(P[2, i] - depth[inter_h, u_real_previous + inter_u])):
-                                    depth[inter_h, u_real_previous +inter_u] = P[2, i]
+                                if (abs(P[2, i] - depth[inter_h, u_real_previous + math.copysign(inter_u, StepWidth)])):
+                                    depth[inter_h, u_real_previous + math.copysign(inter_u, StepWidth)] = P[2, i]
 
 
                     #for inter_u in range(StepWidth):
