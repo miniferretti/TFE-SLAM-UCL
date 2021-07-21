bb1ebf927cbb13e0bdadc6c7fe5e209a4f7e2b53
BruNicoletta <bruno6280@gmail.com>
2021-07-21 15:50:24 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -255,11 +255,11 @@ class MonoDepth_adabin:
                     if(correctionMethod == 1):
                         for inter_u in range(abs(StepWidth)):
                             if ((u_real_previous + inter_u) < 640):
-                                depth[MidHeight, u_real_previous - inter_u ] = depth_previous + (inter_u/StepWidth) * StepDepth
+                                depth[MidHeight, u_real_previous - inter_u ] = depth_previous - (inter_u/StepWidth) * StepDepth
                                 for inter_h in range(image_height):
                                     interDifferenceDepth = depth[MidHeight,u_real_previous -inter_u] - depth[inter_h, u_real_previous +inter_u]
                                     if (inter_h != MidHeight):
-                                        depth[inter_h, u_real_previous +inter_u] = depth[inter_h, u_real_previous +inter_u] + interDifferenceDepth *((image_height - abs(MidHeight - inter_h))/image_height)
+                                        depth[inter_h, u_real_previous +inter_u] = depth[inter_h, u_real_previous +inter_u] - interDifferenceDepth *((image_height - abs(MidHeight - inter_h))/image_height)
                                 #print("depth[MidHeight, u_real_previous + inter_u] = %s" %(depth[MidHeight, u_real_previous + inter_u]))
 
 
