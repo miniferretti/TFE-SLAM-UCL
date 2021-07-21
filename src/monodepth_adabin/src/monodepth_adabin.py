09496adb86d691b08e3daccd6eba80772b715864
BruNicoletta <bruno6280@gmail.com>
2021-07-21 16:08:35 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -255,7 +255,7 @@ class MonoDepth_adabin:
                     if(correctionMethod == 1):
                         for inter_u in range(abs(StepWidth)):
                             if ((u_real_previous + inter_u) < 640):
-                                depth[MidHeight, u_real_previous - inter_u ] = depth_previous - (inter_u/StepWidth) * StepDepth
+                                depth[MidHeight, u_real_previous - inter_u ] = depth_previous + (inter_u/StepWidth) * StepDepth
                                 for inter_h in range(image_height):
                                     interDifferenceDepth = depth[MidHeight,u_real_previous -inter_u] - depth[inter_h, u_real_previous +inter_u]
                                     if (inter_h != MidHeight):
@@ -277,6 +277,17 @@ class MonoDepth_adabin:
                                 if ((u_real_previous - inter_u) < 640):
                                     if(abs(depth[v_real, u_real] - depth[inter_h, u_real_previous - inter_u ]) <= 0.1):
                                         depth[inter_h, u_real_previous - inter_u] = depth[inter_h, u_real_previous - inter_u] + differenceDepth
+
+                    if(correctionMethod == 4):
+                        for inter_u in range(abs(StepWidth)):
+                            if ((u_real_previous + inter_u) < 640):
+                                interDifferenceDepth = (inter_u/StepWidth) * StepDepth
+                                #depth[MidHeight, u_real_previous - inter_u ] = depth_previous + (inter_u/StepWidth) * StepDepth
+                                for inter_h in range(image_height):
+                                    #interDifferenceDepth = depth[MidHeight,u_real_previous -inter_u] - depth[inter_h, u_real_previous +inter_u]
+                                    #if (inter_h != MidHeight):
+                                    depth[inter_h, u_real_previous +inter_u] = depth[inter_h, u_real_previous +inter_u] + interDifferenceDepth *((image_height - abs(MidHeight - inter_h))/image_height)
+                                #print("depth[MidHeight, u_real_previous + inter_u] = %s" %(depth[MidHeight, u_real_previous + inter_u]))
                                     
 
                     
