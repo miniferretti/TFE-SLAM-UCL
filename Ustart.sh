9cb9245e24b8aa4ac6627ac06b96a4f47bfcd8bf
BruNicoletta <bruno6280@gmail.com>
2021-07-25 19:10:50 +0200

    Update Ustart.sh

--- Ustart.sh
+++ Ustart.sh
@@ -6,17 +6,17 @@ catkin_make -DCMAKE_BUILD_TYPE=Release
 source devel/setup.bash 
 
 
-cd src/rp_cyclop
-chmod +x rp_cyclop.py
-cd ..
-cd ..
-cd src/monodepth/src
-chmod +x monodepth.py
-cd ..
-cd ..
-cd ..
-cd src/monodepth_adabin/src
-chmod +x monodepth_adabin.py
+cd src⁩/launch_work_station⁩/launch⁩
+chmod +x launch_work_station_yolo.launch
+#cd ..
+#cd ..
+#cd src/monodepth/src
+#chmod +x monodepth.py
+#cd ..
+#cd ..
+#cd ..
+#cd src/monodepth_adabin/src
+#chmod +x monodepth_adabin.py
 echo "Permission successfuly granted and ready to launch the nodes"
 
 for x in 1 2 3 4 
