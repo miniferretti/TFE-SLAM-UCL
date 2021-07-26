52dbe41f3601b19f23a839c45de825a301857eea
BruNicoletta <bruno6280@gmail.com>
2021-07-26 15:16:40 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -107,11 +107,11 @@ class MonoDepth_adabin:
 
         ranges = np.array([range_data, angle_data], np.float32)
 
-        ranges[0, ranges[0, :] > range_max] = range_max
-        ranges[0, ranges[0, :] < range_min] = range_min
+        #ranges[0, ranges[0, :] > range_max] = range_max
+        #ranges[0, ranges[0, :] < range_min] = range_min
 
-       # for i in range(len(ranges[0, :])):
-       #    print((ranges[0, i], ranges[1, i]))
+        for i in range(len(ranges[0, :])):
+           print((ranges[0, i], ranges[1, i]))
 
         return ranges
 
