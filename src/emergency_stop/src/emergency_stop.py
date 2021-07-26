1c1d235ead880d59e5f4544b137c0c413bf5e1ca
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-07-26 14:19:00 +0200

    test for a custom message

--- src/emergency_stop/src/emergency_stop.py
+++ src/emergency_stop/src/emergency_stop.py
@@ -10,31 +10,47 @@
 #########################################################
 
 
-
 from sensor_msgs.msg import (CameraInfo, CompressedImage, Image, LaserScan,
                              PointCloud2, PointField)
+from emergency_stop.msg import EmergencyStatus
 import rospkg
 import rospy
 import numpy as np
 import sys
 
+
 class Emergency_stop:
     def __init__(self):
 
         # Parameters
-        self.topic_scan = rospy.get_param("~topic_lidar_data","/scan")
-        self.min_dist = rospy.get_param("~min_dist",1)
+
+        self.min_dist = rospy.get_param("~min_dist", 1)
         self.queue_size = rospy.get_param("~queue_size", 2)
 
-        # Publishers
-        self.pub_
+        self.topic_scan = rospy.get_param("~topic_lidar_data", "/scan")
+        self.topic_emergency_statsus = rospy.get_param(
+            "~emergency_topic", "/emergency_message")
 
+        # Publishers
+        self.pub_emergency_status = rospy.Publisher(
+            self.topic_emergency_statsus, EmergencyStatus, queue_size=1)
+
+        # Subscribers
+        self.sub_scan = rospy.Subscriber(
+            self.topic_scan, LaserScan, self.callback)
+
+    def callback(self, scan):
+
+        msg = None
+
+        # Publish the emergency message
+        self.pub_emergency_status.Publish(msg)
 
 
 def main(args):
     rospy.init_node("emergency_stop")
 
-    depth = Emergency_stop()
+    toDo = Emergency_stop()
 
     try:
         rospy.spin()
