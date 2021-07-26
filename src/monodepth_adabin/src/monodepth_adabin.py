d15b625fabf59118233798a1c367b928a900fa1e
BruNicoletta <bruno6280@gmail.com>
2021-07-26 17:56:56 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -447,7 +447,7 @@ class MonoDepth_adabin:
 
         end_time_correction = time.time()
 
-       print("Total time taken  = %s in correction" % (end_time_correction-start_time_correction))
+        print("Total time taken  = %s in correction" % (end_time_correction-start_time_correction))
 
         #return depth
         #depth = correctedDepth.copy()
