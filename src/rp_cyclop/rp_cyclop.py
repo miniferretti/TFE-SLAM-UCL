6793d242580fd14a5d7581801d1ec8999f5e0940
BruNicoletta <bruno6280@gmail.com>
2021-08-05 23:21:47 +0200

    Update rp_cyclop.py

--- src/rp_cyclop/rp_cyclop.py
+++ src/rp_cyclop/rp_cyclop.py
@@ -145,11 +145,12 @@ class image_feature:
         #print("Number of ranges : %s" % (len(ranges[0, :])))
         #print("Number of ranges : %s" % (np.size(ranges, 1)))
         
-        #ranges[0, ranges[0, :] > range_max] = range_max
-        #ranges[0, ranges[0, :] < range_min] = range_min
+        ranges[0, ranges[0, :] > range_max] = range_max
+        ranges[0, ranges[0, :] < range_min] = range_min
 
         #for i in range(len(ranges[0, :])):
            #print((ranges[0, i], ranges[1, i]))
+        
 
         return ranges
 
