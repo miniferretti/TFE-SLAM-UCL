1336f6a53dbcf58729816fccaf09c4027287c01e
BruNicoletta <bruno6280@gmail.com>
2021-08-05 23:12:12 +0200

    Update rp_cyclop.py

--- src/rp_cyclop/rp_cyclop.py
+++ src/rp_cyclop/rp_cyclop.py
@@ -219,7 +219,7 @@ class image_feature:
                         color =  P[2, i] * (1/rangeMax) * 255
                         cv2.circle(image_np, (int(u_real), int(v_real)), 3, (color, 0, (255-color)), -1)
                     if(showUndefinedPoints):
-                        if(P[2, i] = inf):
+                        if(P[2, i] == inf):
                             cv2.circle(image_np, (int(u_real), int(v_real)), 3, (30, 0, 200), -1)
                         else:
                             cv2.circle(image_np, (int(u_real), int(v_real)), 3, (30, 200, 0), -1)
