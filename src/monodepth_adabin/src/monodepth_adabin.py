24bace1d2258271d428335e61a07e4f70583855c
BruNicoletta <bruno6280@gmail.com>
2021-08-13 21:06:10 +0200

    Update monodepth_adabin.py

--- src/monodepth_adabin/src/monodepth_adabin.py
+++ src/monodepth_adabin/src/monodepth_adabin.py
@@ -229,7 +229,7 @@ class MonoDepth_adabin:
         # ---------------------------------------------------------------------------------------------
         # ------    Correcting the image_depth from the data gathered by the LiDAR sensor       ------- 
         #
-        correctionMethod = 9    # Selection of the correction method employed
+        correctionMethod = 10    # Selection of the correction method employed
 
         correctedDepth = np.copy(depth)
 
@@ -249,7 +249,7 @@ class MonoDepth_adabin:
 
             if (u <= U) and (v <= V):
                 if (u >= 0) and (v >= 0) and (P[2, i] >= 0):
-                    u_real = int (self.valmap(u, 0, U, 0, image_width)-13)
+                    u_real = int (self.valmap(u, 0, U, 0, image_width)-14)
                     v_real = int (self.valmap(v, 0, V, 0, image_height))
 
                     differenceDepth =  P[2, i] - depth[v_real, u_real]
