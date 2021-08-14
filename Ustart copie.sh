1636ce633e8e16354afdd93ad855f21e3e4dad6a
BruNicoletta <bruno6280@gmail.com>
2021-08-14 08:10:45 +0200

    Create Ustart copie.sh

---
+++ Ustart copie.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+git pull
+
+catkin_make -DCMAKE_BUILD_TYPE=Release
+
+source devel/setup.bash 
+
+
+cd src⁩/launch_work_station⁩/launch⁩
+chmod +x launch_work_station_yolo.launch
+#cd ..
+#cd ..
+#cd src/monodepth/src
+#chmod +x monodepth.py
+#cd ..
+#cd ..
+#cd ..
+#cd src/monodepth_adabin/src
+#chmod +x monodepth_adabin.py
+echo "Permission successfuly granted and ready to launch the nodes"
+
+for x in 1 2 3 4 
+do
+    echo "."
+    sleep 1
+done
+
+roslaunch launch_work_station launch_work_station_orb_slam2_rgbd.launch
