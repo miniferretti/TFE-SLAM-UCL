7840132f2933246c7f86c17efd2d4fdc4ef93a8d
miniferretti <36267317+miniferretti@users.noreply.github.com>
2021-09-01 16:15:32 +0200

    test

---
+++ src/emergency_stop/src/Kdtree.py
@@ -0,0 +1,140 @@
+import math
+import numpy as np
+
+
+class Node:
+    def __init__(self, data, point_id):
+        self.point = {"R": data[0], "T": data[1]}
+        self.point_id = point_id
+        self.left_node = None
+        self.right_node = None
+
+
+class KdTree_class:
+    def __init__(self):
+        self.root = None
+        self.kdtree_display_dict = dict()
+
+    def insert_points(self, scan_dataframe, display_output=False):
+        """
+        :param pcd_dataframe: dataframe containing (R,Theta) coordinates data
+        :return:
+        """
+        for row in scan_dataframe:
+            range, angle, point_id = row[0], row[1], np.where(
+                scan_dataframe == row)
+            point = (range, angle)
+            level = 0
+            self.root = self.build_kdtree(self.root, level, point, point_id)
+        # If display_output is enabled, then display the contents of Kdtree
+        if display_output:
+            print("Kdtree Build Complete")
+            self.display_kdtree(self.root)
+            for pair in self.kdtree_display_dict.items():
+                print(f"Depth = {pair[0]}, Points = {pair[1]} ")
+        return self.root
+
+    def build_kdtree(self, node, depth, point, point_id):
+        """
+        :param node: Node class object
+        :param depth: level0 -Radius, level1-Theta
+        :return: root node
+        """
+        if node is None:
+            node = Node(point, point_id)
+            return node
+
+        # If current node is empty, then assign the point as root
+        current_node = Node(point, point_id)
+        # Level should always be within 0-2 range
+        depth %= 2
+        # levels correspond to (X,Y,Z), check at each level before assigning left/right to the root node
+        # self.__dict_key returns X,Y,Z based on the level value. Check function for detailed description
+        if node.point[self.__dict_key(depth)] < current_node.point[self.__dict_key(depth)]:
+            # If value at level less than current node point, add it as a right node
+            node.right_node = self.build_kdtree(
+                node.right_node, depth + 1, point, point_id)
+        else:
+            # If value at level is more than current node point, add it as a left node
+            node.left_node = self.build_kdtree(
+                node.left_node, depth + 1, point, point_id)
+        return node
+
+    def search_elements(self, node, search_point, distance_threshold, depth=0, kdtree_search_results=set()):
+        """
+        :param node: node of kdtree
+        :param search_point: (x,y,z) point
+        :param distance_threshold: pcd elements near point
+        :param depth: level of the kdtree depth
+        :param kdtree_search_results: level of the kdtree depth
+        :return: ids which can be considered as near points
+        """
+        depth %= 2
+        current_node = node
+        if current_node is not None:
+            # If current node is within the distance threshold of search_point
+            if(((current_node.point["R"] < search_point[0] + distance_threshold) and (current_node.point["R"] > search_point[0] - distance_threshold)) and
+               ((current_node.point["T"] < search_point[1] + distance_threshold) and (current_node.point["T"] > search_point[1] - distance_threshold))):
+                # Calculate the distance of search point from current node
+                point_distance = math.sqrt(math.pow(current_node.point["R"], 2) + math.pow(
+                    search_point[0], 2)-2*current_node.point["R"]*search_point[0]*math.cos(current_node.point["T"]-search_point[1]))
+                if point_distance <= distance_threshold:
+                    kdtree_search_results.add(current_node.point_id)
+                else:
+                    pass
+            # Iterate recursively
+            # if current_node.point[self.__dict_key(depth)] < search_point[depth]:
+            if current_node.point[self.__dict_key(depth)] < search_point[depth] + distance_threshold:
+                self.search_elements(current_node.right_node, search_point, distance_threshold,
+                                     depth+1, kdtree_search_results)
+
+            # else:
+            if current_node.point[self.__dict_key(depth)] > search_point[depth] - distance_threshold:
+                self.search_elements(current_node.left_node, search_point, distance_threshold,
+                                     depth+1, kdtree_search_results)
+            return kdtree_search_results
+
+    def display_kdtree(self, node, depth=0):
+        """
+        updates the self.kdtree_dict with the points are corresponding depth
+        :param node: root node
+        :param depth: indicates the depth of Kdtree
+        """
+        current_node = node
+        try:
+            # If there are values already present, append the list with the point.
+            self.kdtree_display_dict[depth].extend([(current_node.point["R"],
+                                                     current_node.point["T"])])
+        except KeyError:
+            # If there are no values at the level, add value as first point
+            self.kdtree_display_dict[depth] = [(current_node.point["R"],
+                                                current_node.point["T"])]
+        # Run the recursion until a function hits the empty node
+        if current_node is not None:
+            # Check
+            if current_node.left_node is not None:
+                left_node = current_node.left_node
+                # increment the value of depth
+                depth += 1
+                # at every node, call the recursive function
+                self.display_kdtree(left_node, depth)
+
+            if current_node.right_node is not None:
+                right_node = current_node.right_node
+                # increment the value of depth
+                depth += 1
+                # at every node, call the recursive function
+                self.display_kdtree(right_node, depth)
+
+    @staticmethod
+    def __dict_key(number):
+        """
+        returns R,Theta based on number
+        :param number: 0,1
+        :return: R,Theta
+        """
+        try:
+            key_dict = {0: "R", 1: "T"}
+            return str(key_dict[number])
+        except KeyError:
+            raise Exception(f"Incorrect Level({number}) Assignment.")
---
+++ src/emergency_stop/src/emergency_script.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+#########################################################
+# Failsafe node to avoid unseen obstacles by
+# the depth perception algorithm (Monodepth_adabin)
+# of the drone.
+# This node subscribes to scan messages from the LiDar
+# sensor.
+#
+# Author: Matteo Ferretti
+# Date: 25/7/2021
+#########################################################
+
+
+from sensor_msgs.msg import (CameraInfo, CompressedImage, Image, LaserScan,
+                             PointCloud2, PointField)
+from emergency_stop.msg import EmergencyStatus
+import rospkg
+import rospy
+import numpy as np
+import sys
+import time
+import math
+import time
+sys.setrecursionlimit(10**8)
+
+
+class Emergency_stop:
+    def __init__(self):
+
+        # Parameters
+
+        self.min_dist = rospy.get_param("~min_dist", 1)
+        self.threshold = rospy.get_param("~threshold", 0.1)
+        self.queue_size = rospy.get_param("~queue_size", 1)
+
+        self.topic_scan = rospy.get_param("~topic_lidar_data", "/scan")
+        self.topic_emergency_statsus = rospy.get_param(
+            "~emergency_topic", "/emergency_message")
+
+        # Publishers
+        self.pub_emergency_status = rospy.Publisher(
+            self.topic_emergency_statsus, EmergencyStatus, queue_size=1)
+
+        # Subscribers
+        self.sub_scan = rospy.Subscriber(
+            self.topic_scan, LaserScan, self.callback)
+
+    def range_filter(self, scan_sync):
+
+        range_data = np.array(scan_sync.ranges, np.float32)
+        angle_min = scan_sync.angle_min
+        angle_max = scan_sync.angle_max
+        range_min = np.min(range_data)
+        range_max = np.max(range_data)
+        angle_increment = scan_sync.angle_increment
+        N = int((angle_max-angle_min)/angle_increment)
+        angle_data = np.linspace(angle_min, angle_max, num=(N+1)) + math.pi
+        # print(len(angle_data))
+        # print(len(range_data))
+
+        ranges = np.array([range_data, angle_data], np.float32)
+
+        ranges[0, ranges[0, :] > range_max] = range_max
+        ranges[0, ranges[0, :] < range_min] = range_min
+
+        # for i in range(len(ranges[0, :])):
+        #    print((ranges[0, i], ranges[1, i]))
+
+        return ranges
+
+    def callback(self, scan):
+
+        ranges = self.range_filter(scan)
+        msg = EmergencyStatus()
+        msg.header = rospy.Time.now()
+
+        obs = ranges[:, ranges[0, :] <= self.min_dist]
+
+        n_obs = 0
+        if obs.size != 0:
+            n = 0
+            prev = None
+            n_obs = 1
+            for col in obs:
+                if n == 0:
+                    prev = col
+                    n += 1
+                else:
+
+                    dist = math.sqrt(math.pow(
+                        col[0], 2) + math.pow(prev[0], 2)-2*col[0]*prev[0]*math.cos(col[1]-prev[1]))
+
+                    if dist > self.threshold:
+                        n_obs += 1
+                    prev = col
+            msg.obstacle_detected = True
+            msg.num_obs = n_obs
+            msg.ranges = obs[0]
+            msg.angles = obs[1]
+        else:
+            msg.obstacle_detected = False
+        print("Number of obstacle detected {}".format(obs))
+        # Publish the emergency message
+        self.pub_emergency_status.Publish(msg)
+
+
+def main(args):
+    rospy.init_node("emergency_stop")
+
+    toDo = Emergency_stop()
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down ROS Image depth sensing module")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
